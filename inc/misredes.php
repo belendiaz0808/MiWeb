e8f438fadb548f848dcd57d985e9616369305826
belendiaz0808 <belendiazsoler0808@gmail.com>
2018-02-21 13:21:23 +0100

    Revert "Revert "redes.php""
    This reverts commit 4c430c741601799412cc41fe8be0b528836d1dc0.

---
+++ inc/misredes.php
@@ -0,0 +1,17 @@
+			
+			<?php include("inc/head.php");?>
+		<div id="content" class="container-fluid">
+			<div class="row">
+				<div class="col-4">	
+					<div>
+						<i class="fab fa-twitter"></i>
+						<i class="fab fa-facebook-square"></i>
+						<i class="fab fa-youtube"></i>
+						<i class="fab fa-spotify"></i>
+						<i class="fab fa-instagram"></i>
+						<i class="fab fa-blogger"></i>
+					</div>
+				</div>
+			</div>
+		</div>
+			<?php include("inc/footer.php");  ?> 
